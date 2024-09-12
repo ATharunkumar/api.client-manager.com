5870aca380b0ffe81de618a29eabd1730ca7354e
@code.labsquire <code@labsquire.com>
2024-09-12 20:47:50 +0530

    TA Add schemas

--- src/schemas/clients.ts
+++ src/schemas/clients.ts
@@ -1,22 +1,25 @@
-import { index, numeric, pgEnum, pgTable, serial, text, timestamp, varchar } from 'drizzle-orm/pg-core';
+import { relations } from 'drizzle-orm';
+import { boolean, index, numeric, pgEnum, pgTable, serial, text, timestamp, varchar } from 'drizzle-orm/pg-core';
+import { services } from './services';
+import { invoiceFiles } from './invoicefiles';
 export const statusEnum = pgEnum('status', ['ACTIVE', 'INACTIVE']);
 
 
 export const clients = pgTable('clients', {
     id: serial('id').primaryKey(),
-    name: text('name').notNull(),
-    type: text('type').notNull(),
-    poc: text('poc').notNull(),
-    role: text('role').notNull(),
-    email: text('email').notNull().unique(),
-    phone: text('phone').notNull(),
-    secondaryPhone: varchar('secondary_phone').notNull(),
+    name: varchar('name').notNull(),
+    poc: varchar('poc').notNull(),
+    role: varchar('role'),
+    email: varchar('email').notNull().unique(),
+    phone: varchar('phone').notNull(),
+    secondaryPhone: varchar('secondary_phone'),
     status: statusEnum('status').default("ACTIVE"),
     remarks: text('remarks'),
     bussinessUrl: text('bussiness_url'),
     address: text('address'),
     state: varchar('state'),
     city: varchar('city'),
+    gst: boolean('gst').default(false),
     country: varchar('country'),
     totalInvoiceAmount: numeric('total_invoice_amount', { precision: 100, scale: 2 }),
     createdAt: timestamp('created_at').notNull().defaultNow(),
@@ -24,9 +27,19 @@ export const clients = pgTable('clients', {
 
 }, (table: any) => {
     return {
-        nameIdx: index("name_idx").on(table.name)
+        nameIdx: index("name_idx").on(table.name),
+        statusIdx: index("client_status_idx").on(table.status)
     };
 });
+
+
+export const clientWithServicesRelations = relations(clients, ({ many }) => ({
+    services: many(services),
+}));
+
+export const clientWithFilesRelations = relations(clients, ({ many }) => ({
+    invoiceFiles: many(invoiceFiles),
+}));
 
 
 export type client = typeof clients.$inferSelect; // return type when queried
